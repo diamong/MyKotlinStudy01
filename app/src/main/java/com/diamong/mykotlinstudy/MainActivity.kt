66ee48919893b17fd4b4b726e6c4cd67dbeeb5ac
diamong01 <45653307+diamong01@users.noreply.github.com>
2019-05-29 12:46:35 +0900

    Section Two, Video Two

--- app/src/main/java/com/diamong/mykotlinstudy/MainActivity.kt
+++ app/src/main/java/com/diamong/mykotlinstudy/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.diamong.mykotlinstudy
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            Log.i("MainActivity","Button Clicked")
+            Toast.makeText(applicationContext,"Button was Clicked!!!!!",Toast.LENGTH_LONG).show()
+        }
+
     }
+
 }
