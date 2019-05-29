aed8d7c7fbdea43e81ecf5684d479fd7f74e6bd3
diamong01 <45653307+diamong01@users.noreply.github.com>
2019-05-29 15:23:27 +0900

    Section Two, Video Three

--- app/src/main/java/com/diamong/mykotlinstudy/MainActivity.kt
+++ app/src/main/java/com/diamong/mykotlinstudy/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.diamong.mykotlinstudy
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
@@ -15,6 +16,15 @@ class MainActivity : AppCompatActivity() {
         button.setOnClickListener {
             Log.i("MainActivity","Button Clicked")
             Toast.makeText(applicationContext,"Button was Clicked!!!!!",Toast.LENGTH_LONG).show()
+        }
+
+        button2.setOnClickListener {
+            val message: String =et_user_message.text.toString()
+            Toast.makeText(this,message,Toast.LENGTH_LONG).show()
+
+            val intent = Intent(this,SecondActivity::class.java)
+            startActivity(intent)
+
         }
 
     }
